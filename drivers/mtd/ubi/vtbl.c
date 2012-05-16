0b29c93966a391fcd6e44c45f54d68a5813ee63c
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2012-05-16 18:09:08 +0300

    UBI: remove Kconfig debugging option
    This patch kills the UBI debugging Kconfig option completely and makes all the
    debugging stuff to be always compiled-in. It was pain in the neck to maintain
    this useless option because all users I am aware of have debugging enabled
    anyway - how else will you diagnose errors otherwise?
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/ubi/vtbl.c
+++ drivers/mtd/ubi/vtbl.c
@@ -62,11 +62,7 @@
 #include <asm/div64.h>
 #include "ubi.h"
 
-#ifdef CONFIG_MTD_UBI_DEBUG
 static void paranoid_vtbl_check(const struct ubi_device *ubi);
-#else
-#define paranoid_vtbl_check(ubi)
-#endif
 
 /* Empty volume table record */
 static struct ubi_vtbl_record empty_vtbl_record;
@@ -860,8 +856,6 @@ out_free:
 	return err;
 }
 
-#ifdef CONFIG_MTD_UBI_DEBUG
-
 /**
  * paranoid_vtbl_check - check volume table.
  * @ubi: UBI device description object
@@ -876,5 +870,3 @@ static void paranoid_vtbl_check(const struct ubi_device *ubi)
 		BUG();
 	}
 }
-
-#endif /* CONFIG_MTD_UBI_DEBUG */
